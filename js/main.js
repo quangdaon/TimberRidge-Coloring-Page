8b67af171acef7ea7f4716d4848e690cf3fd8903
Quangdao Nguyen <quangdaonguyen@marcushotels.com>
2015-08-12 15:58:33 -0500

    Minor Fade Animation
    Add a tiny fade effect to dialog popups :)

--- js/main.js
+++ js/main.js
@@ -10,190 +10,225 @@
  */
 (function() {
 
-	var docElem = window.document.documentElement,
-		// transition end event name
-		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
+    var docElem = window.document.documentElement,
+        // transition end event name
+        transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
 
-	function scrollX() { return window.pageXOffset || docElem.scrollLeft; }
-	function scrollY() { return window.pageYOffset || docElem.scrollTop; }
-	function getOffset(el) {
-		var offset = el.getBoundingClientRect();
-		return { top : offset.top + scrollY(), left : offset.left + scrollX() };
-	}
+    function scrollX() {
+        return window.pageXOffset || docElem.scrollLeft;
+    }
 
-	function dragMoveListener(event) {
-		var target = event.target,
-			// keep the dragged position in the data-x/data-y attributes
-			x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-			y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
 
-		// translate the element
-		target.style.transform = target.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
-		target.style.zIndex = 10000;
+    function getOffset(el) {
+        var offset = el.getBoundingClientRect();
+        return {
+            top: offset.top + scrollY(),
+            left: offset.left + scrollX()
+        };
+    }
 
-		// update the posiion attributes
-		target.setAttribute('data-x', x);
-		target.setAttribute('data-y', y);
-	}
+    function dragMoveListener(event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-	function revertDraggable(el) {
-		el.style.transform = el.style.webkitTransform = 'none';
-		el.style.zIndex = 1;
-		el.setAttribute('data-x', 0);
-		el.setAttribute('data-y', 0);
-	}
+        // translate the element
+        target.style.transform = target.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
+        target.style.zIndex = 10000;
 
-	function init() {
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
 
+    function revertDraggable(el) {
+        el.style.transform = el.style.webkitTransform = 'none';
+        el.style.zIndex = 1;
+        el.setAttribute('data-x', 0);
+        el.setAttribute('data-y', 0);
+    }
 
-
-		document.querySelector('button.info-close').addEventListener('click', function() {
-			var info = document.querySelector('.info-wrap');
-			// alert(info.parentNode);
-			info.parentNode.removeChild(info);
-		});
-
-		// target elements with the "drag-element" class
-		interact('.drag-element').draggable({
-			// enable inertial throwing
-			inertia: true,
-			// call this function on every dragmove event
-			onmove: dragMoveListener,
-			onend: function (event) {
-				if(!classie.has(event.target, 'drag-element--dropped') && !classie.has(event.target, 'drag-element--dropped-text')) {
-					revertDraggable(event.target);
-				}
-			}
-		});
-
-		// enable draggables to be dropped into this
-		interact('.paint-area').dropzone({
-			// only accept elements matching this CSS selector
-			accept: '.drag-element',
-			// Require a 75% element overlap for a drop to be possible
-			overlap: 0.75,
-			ondragenter: function (event) {
-				classie.add(event.target, 'paint-area--highlight');
-				$('path').css('stroke', event.relatedTarget.getAttribute('data-color'));
-			},
-			ondragleave: function (event) {
-				classie.remove(event.target, 'paint-area--highlight');
-			},
-			ondrop: function (event) {
-				var type = 'area';
-				if(classie.has(event.target, 'paint-area--text')) {
-					type = 'text';
-				}
-
-				var draggableElement = event.relatedTarget;
-
-				classie.add(draggableElement, type === 'text' ? 'drag-element--dropped-text' : 'drag-element--dropped');
-
-				var onEndTransCallbackFn = function(ev) {
-					this.removeEventListener( transEndEventName, onEndTransCallbackFn );
-					if( type === 'area' ) {
-						paintArea(event.dragEvent, event.target, draggableElement.getAttribute('data-color'));
-					}
-					setTimeout(function() {
-						revertDraggable(draggableElement);
-						classie.remove(draggableElement, type === 'text' ? 'drag-element--dropped-text' : 'drag-element--dropped');
-					}, type === 'text' ? 0 : 250);
-				};
-				if( type === 'text' ) {
-					paintArea(event.dragEvent, event.target, draggableElement.getAttribute('data-color'));
-				}
-				draggableElement.querySelector('.drop').addEventListener(transEndEventName, onEndTransCallbackFn);
-			},
-			ondropdeactivate: function (event) {
-				// remove active dropzone feedback
-				classie.remove(event.target, 'paint-area--highlight');
-			}
-		});
-
-		// reset colors
-
-		
-		document.querySelector('button.reset-button').addEventListener('click', function(){
-				// alert("The code broke :p");
-				var resetBoxContainer = document.createElement('div');
-				resetBoxContainer.setAttribute('class', 'reset-confirm-wrap dialog-wrap');
-				resetBoxContainer.setAttribute('id', 'reset-confirm');
-
-				var resetBox = document.createElement('div');
-				resetBox.setAttribute('class', 'reset-confirm dialog');
-
-				var resetHeader = document.createElement('h3');
-				resetHeader.appendChild(document.createTextNode('Timber Ridge Coloring Page'));
-
-				var resetWarning = document.createElement('p');
-				resetWarning.appendChild(document.createTextNode('Are you sure you want to reset the colors?'));
-
-				var resetCancelButton = document.createElement('button');
-				resetCancelButton.setAttribute('class', 'reset-confirm-cancel');
-				resetCancelButton.appendChild(document.createTextNode('No. Sowiezz'));
-
-				var resetConfirmButton = document.createElement('button');
-				resetConfirmButton.setAttribute('class', 'reset-confirm-submit');
-				resetConfirmButton.appendChild(document.createTextNode('Yesh pweez c:'));
-
-				resetBox.appendChild(resetHeader);
-				resetBox.appendChild(resetWarning);
-				resetBox.appendChild(resetCancelButton);
-				resetBox.appendChild(resetConfirmButton);
-
-				resetBoxContainer.appendChild(resetBox);
-
-				document.querySelector('.content').appendChild(resetBoxContainer);
+    function init() {
 
 
-				document.querySelector('button.reset-confirm-cancel').addEventListener('click', function() {
-					var reset = document.querySelector('.reset-confirm-wrap');
-					reset.parentNode.removeChild(reset);
-				});
-				
-				document.querySelector('button.reset-confirm-submit').addEventListener('click', function() {
-					resetColors();
-					var reset = document.querySelector('.reset-confirm-wrap');
-					reset.parentNode.removeChild(reset);
-				});
-		});
-	}
 
-	function paintArea(ev, el, color) {
-			el.setAttributeNS(null, 'fill', color);
-	}
+        document.querySelector('button.info-close').addEventListener('click', function() {
+            var info = document.querySelector('.info-wrap');
+            // alert(info.parentNode);
+            $(info).animate({
+                opacity: 0
+            }, 50, function() {
+                info.parentNode.removeChild(info);
+            });
+        });
 
-	function resetColors(elem) {
-		[].slice.call(document.querySelectorAll('.paint-area')).forEach(function(el) {
-			el.style[classie.has(el, 'paint-area--text') ? 'color' : 'background-color'] = '';
-			el.setAttributeNS(null, 'fill', '#FFFFFF');
-		});
-	}
+        // target elements with the "drag-element" class
+        interact('.drag-element').draggable({
+            // enable inertial throwing
+            inertia: true,
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            onend: function(event) {
+                if (!classie.has(event.target, 'drag-element--dropped') && !classie.has(event.target, 'drag-element--dropped-text')) {
+                    revertDraggable(event.target);
+                }
+            }
+        });
 
-	init();
+        // enable draggables to be dropped into this
+        interact('.paint-area').dropzone({
+            // only accept elements matching this CSS selector
+            accept: '.drag-element',
+            // Require a 75% element overlap for a drop to be possible
+            overlap: 0.75,
+            ondragenter: function(event) {
+                classie.add(event.target, 'paint-area--highlight');
+                $('path').css('stroke', event.relatedTarget.getAttribute('data-color'));
+            },
+            ondragleave: function(event) {
+                classie.remove(event.target, 'paint-area--highlight');
+            },
+            ondrop: function(event) {
+                var type = 'area';
+                if (classie.has(event.target, 'paint-area--text')) {
+                    type = 'text';
+                }
+
+                var draggableElement = event.relatedTarget;
+
+                classie.add(draggableElement, type === 'text' ? 'drag-element--dropped-text' : 'drag-element--dropped');
+
+                var onEndTransCallbackFn = function(ev) {
+                    this.removeEventListener(transEndEventName, onEndTransCallbackFn);
+                    if (type === 'area') {
+                        paintArea(event.dragEvent, event.target, draggableElement.getAttribute('data-color'));
+                    }
+                    setTimeout(function() {
+                        revertDraggable(draggableElement);
+                        classie.remove(draggableElement, type === 'text' ? 'drag-element--dropped-text' : 'drag-element--dropped');
+                    }, type === 'text' ? 0 : 250);
+                };
+                if (type === 'text') {
+                    paintArea(event.dragEvent, event.target, draggableElement.getAttribute('data-color'));
+                }
+                draggableElement.querySelector('.drop').addEventListener(transEndEventName, onEndTransCallbackFn);
+            },
+            ondropdeactivate: function(event) {
+                // remove active dropzone feedback
+                classie.remove(event.target, 'paint-area--highlight');
+            }
+        });
+
+        // reset colors
+
+
+        document.querySelector('button.reset-button').addEventListener('click', function() {
+            // alert("The code broke :p");
+            var resetBoxContainer = document.createElement('div');
+            resetBoxContainer.setAttribute('class', 'reset-confirm-wrap dialog-wrap');
+            resetBoxContainer.setAttribute('id', 'reset-confirm');
+
+            var resetBox = document.createElement('div');
+            resetBox.setAttribute('class', 'reset-confirm dialog');
+
+            var resetHeader = document.createElement('h3');
+            resetHeader.appendChild(document.createTextNode('Timber Ridge Coloring Page'));
+
+            var resetWarning = document.createElement('p');
+            resetWarning.appendChild(document.createTextNode('Are you sure you want to reset the colors?'));
+
+            var resetCancelButton = document.createElement('button');
+            resetCancelButton.setAttribute('class', 'reset-confirm-cancel');
+            resetCancelButton.appendChild(document.createTextNode('No. Sowiezz'));
+
+            var resetConfirmButton = document.createElement('button');
+            resetConfirmButton.setAttribute('class', 'reset-confirm-submit');
+            resetConfirmButton.appendChild(document.createTextNode('Yesh pweez c:'));
+
+            resetBox.appendChild(resetHeader);
+            resetBox.appendChild(resetWarning);
+            resetBox.appendChild(resetCancelButton);
+            resetBox.appendChild(resetConfirmButton);
+
+            resetBoxContainer.appendChild(resetBox);
+            resetBoxContainer.style.opacity = 0;
+                document.querySelector('.content').appendChild(resetBoxContainer);
+
+            $(resetBoxContainer).animate({
+                opacity: 1
+            }, 50);
+
+
+
+
+            document.querySelector('button.reset-confirm-cancel').addEventListener('click', function() {
+                var reset = document.querySelector('.reset-confirm-wrap');
+
+	            $(reset).animate({
+	                opacity: 0
+	            }, 50, function() {
+                	reset.parentNode.removeChild(reset);
+	            });
+            });
+
+            document.querySelector('button.reset-confirm-submit').addEventListener('click', function() {
+                resetColors();
+                var reset = document.querySelector('.reset-confirm-wrap');
+	            $(reset).animate({
+	                opacity: 0
+	            }, 50, function() {
+                	reset.parentNode.removeChild(reset);
+	            });
+            });
+        });
+    }
+
+    function paintArea(ev, el, color) {
+        el.setAttributeNS(null, 'fill', color);
+    }
+
+    function resetColors(elem) {
+        [].slice.call(document.querySelectorAll('.paint-area')).forEach(function(el) {
+            el.style[classie.has(el, 'paint-area--text') ? 'color' : 'background-color'] = '';
+            el.setAttributeNS(null, 'fill', '#FFFFFF');
+        });
+    }
+
+    init();
 
 })();
 
 for (var i = 0; i <= $(".color-swatch").length; i++) {
-	var colorParent = $(".color-swatch")[i];
-	var fillColor = function(){return $(colorParent).find(".drag-element").data("color")}
-	$(colorParent).css("background-color", fillColor());
-	$(colorParent).find(".cno").css("background-color", fillColor());
+    var colorParent = $(".color-swatch")[i];
+    var fillColor = function() {
+        return $(colorParent).find(".drag-element").data("color")
+    }
+    $(colorParent).css("background-color", fillColor());
+    $(colorParent).find(".cno").css("background-color", fillColor());
 };
 
-$('body').keyup(function(event){
-    if(event.keyCode == 13){
+$('body').keyup(function(event) {
+    if (event.keyCode == 13) {
         $(".info-close").click();
     }
 });
 
-$('.paint-area').dblclick(function(){
-	clrColor(this);
-	// this.setAttributeNS(null, 'fill', '#f00');
+$('.paint-area').dblclick(function() {
+    clrColor(this);
+    // this.setAttributeNS(null, 'fill', '#f00');
 });
 
-clrColor = function(pathElement){
-	pathElement.setAttributeNS(null, 'fill', "#FFFFFF");
-	$(".paint-area").removeClass("paint--active");
+clrColor = function(pathElement) {
+    pathElement.setAttributeNS(null, 'fill', "#FFFFFF");
 }
